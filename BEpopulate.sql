095a000de82b0c2bf8309c13b1d0e0b3401681c4
Abby Garrett <agarrett@go.olemiss.edu>
2018-09-29 13:27:18 -0500

    Update BEpopulate.sql

--- BEpopulate.sql
+++ BEpopulate.sql
@@ -1,9 +1,20 @@
 -- populates tables
+sp_configure 'show advanced options', 1;
+RECONFIGURE;
+GO  --Added
+sp_configure 'Ad Hoc Distributed Queries', 1;
+RECONFIGURE;
+GO
+
 INSERT INTO Host VALUES (1,  "Kate", "Brown", "6628010399", NULL, "katebrown@gmail.com", "Julia", "Brown", "6623548172", "jbrown@gmail.com");
 
 INSERT INTO Transportation VALUES ("Van", 1), ("Bus", 2);
 
-INSERT INTO Projects VALUES (1, 1, "692 Olive Branch Road, Oxford, MS, 38655", 4, 8, NULL, 2, "Landscaping", NULL, "Go down 7", "no", NULL, "Exposure to outdoor allergens (grass, pollen, etc.)");
+INSERT INTO proj_restriction VALUES (1, "Paint"), (2, "Powertools"), (3, "Medium Lifting"), (4, "Heavy Lifting"), (5, "Exposure to outdoor allergens (grass, pollen, etc.)"), (6, "Chemicals"), (7, "Climbing"), (8, "Animals");
+
+INSERT INTO proj_category VALUES (1, "Nonprofit"), (2, "Chruch"), (3, "Government or Government Agency"), (4, "Residential, yard work"), (5, "Residential, basic renovations"), (6, "Residential, cleaning"), (7, "Small Business"), (8, "K-12 School"), (9, "University");
+
+INSERT INTO Projects VALUES (1, 1, "692 Olive Branch Road, Oxford, MS, 38655", 4, 8, NULL, 2, "Landscaping", NULL, "Go down 7", "no", NULL, 5);
 
 INSERT INTO Committee_Members VALUES (1, "Abby", "Garrett"), (2, "Sarah", "Smith"), (3, "Marguerite", "Marquez");
 
@@ -14,4 +25,4 @@ LOAD DATA LOCAL INFILE "/home/agarrett/organizationsCSV.csv" INTO TABLE Organiza
 
 INSERT INTO Team_Leaders VALUES (1, "Hunter", "Johnson", "6016240922", "hjohnson@go.com", 1), (2, "Haleigh", "Hunter", "7314137347", "hurt@gmail.com", 1), (3, "Abby", "Garrett", "2053545133", "agarrett@gmail.com", 1);
 
-INSERT INTO volunteers VALUES (1, "Libby", "Weir", "yes", NULL, "8325159977", 1, 1), (2,"Alexander", "Roy", "no", NULL, "6155122899", NULL, 1), (3, "Neely", "Francis", "yes", NULL, "2059945888", 1, 1);
+INSERT INTO volunteers VALUES (1, "Libby", "Weir", "libbyw@gmail.com", "yes", NULL, "8325159977", 1, 1), (2,"Alexander", "Roy", "avgroy@gmail.com", "no", NULL, "6155122899", NULL, 1), (3, "Neely", "Francis", "fran.nel@gmail.com", "yes", NULL, "2059945888", 1, 1);
