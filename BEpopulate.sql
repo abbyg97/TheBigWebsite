78d2043a1a68e1e12c8d9f82273d43e6c9f825e1
Abby Garrett <agarrett@go.olemiss.edu>
2018-10-01 09:05:16 -0500

    Update BEpopulate.sql

--- BEpopulate.sql
+++ BEpopulate.sql
@@ -10,15 +10,15 @@ INSERT INTO Host VALUES (1,  "Kate", "Brown", "6628010399", NULL, "katebrown@gma
 
 INSERT INTO Transportation VALUES ("Van", 1), ("Bus", 2);
 
-INSERT INTO proj_restriction VALUES (1, "Paint"), (2, "Powertools"), (3, "Medium Lifting"), (4, "Heavy Lifting"), (5, "Exposure to outdoor allergens (grass, pollen, etc.)"), (6, "Chemicals"), (7, "Climbing"), (8, "Animals");
+-- INSERT INTO proj_restriction VALUES (1, "Paint"), (2, "Powertools"), (3, "Medium Lifting"), (4, "Heavy Lifting"), (5, "Exposure to outdoor allergens (grass, pollen, etc.)"), (6, "Chemicals"), (7, "Climbing"), (8, "Animals");
 
 INSERT INTO proj_category VALUES (1, "Nonprofit"), (2, "Chruch"), (3, "Government or Government Agency"), (4, "Residential, yard work"), (5, "Residential, basic renovations"), (6, "Residential, cleaning"), (7, "Small Business"), (8, "K-12 School"), (9, "University");
 
-INSERT INTO Projects VALUES (1, 1, "692 Olive Branch Road, Oxford, MS, 38655", 4, 8, NULL, 2, "Landscaping", NULL, "Go down 7", "no", NULL, 5);
+INSERT INTO Projects VALUES (1, 1, "692 Olive Branch Road, Oxford, MS, 38655", 4, 8, NULL, 2, "Landscaping", NULL, "Go down 7", "no", NULL, "Painting");
 
 INSERT INTO Committee_Members VALUES (1, "Abby", "Garrett"), (2, "Sarah", "Smith"), (3, "Marguerite", "Marquez");
 
-INSERT INTO Approvals VALUES (1, 1, "no", "none", 1), (1, 1, "no", "none", 2), (1, 2, "no", "none", 3);
+INSERT INTO Approvals VALUES (1, 1, "no", "none", 1), (2, 1, "no", "none", 2), (3, 2, "no", "none", 3);
 
 --https://stackoverflow.com/questions/3635166/how-to-import-csv-file-to-mysql-table
 LOAD DATA LOCAL INFILE "/home/agarrett/organizationsCSV.csv" INTO TABLE Organization FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' (number, name, total_members);
