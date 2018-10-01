90b8621c021fcaa09247addc4ae6c08529d66afb
Abby Garrett <agarrett@go.olemiss.edu>
2018-10-01 09:11:12 -0500

    Update viewHostProjects.php

--- viewHostProjects.php
+++ viewHostProjects.php
@@ -1,7 +1,7 @@
 <?php
 	require_once("included_functions.php");
   require_once("session.php");
-	new_header("Here are your volunteers!");
+	new_header("Here are your projects!");
 	$mysqli = db_connection();
 	if (($output = message()) !== null) {
 		echo $output;
@@ -16,10 +16,10 @@
 		$last=$row["last_name"];
 	}
 
-  $query="SELECT address, min_volunteers AS 'min', ";
+  $query="SELECT Project_Number, address, min_volunteers AS 'min', ";
   $query.="max_volunteers AS 'max', description, tools, additional_comments AS 'comments', ";
   $query.="rain, rain_proj, ";
-  $query.="proj_restriction.restriction AS 'restrictions', proj_category.cat AS 'category' from Projects join proj_restriction on Projects.restriction_violation=proj_restriction.number ";
+  $query.="restriction_violation AS 'restrictions', proj_category.cat AS 'category' from Projects ";
   $query.="join proj_category on Projects.category=proj_category.number ";
   $query.="join Host on Projects.Host=Host.host_number WHERE Host.host_number=".$host_num;
 	$result = $mysqli->query($query);
@@ -31,7 +31,7 @@
 		echo "<center>";
     echo "<h2>Here are all projects for ".$first." ".$last."</h2>";
     echo "<table>";
-    echo "<tr><th>Address</th><th>Minimum Volunteers</th><th>Maximum Volunteers</th><th>Description</th><th>Tools requested</th><th>Comments</th><th>Come if it rains?</th><th>Describe the project we would do on a rainy day</th><th>Restrictions</th><th>Category</th></tr>";
+    echo "<tr><th>Address</th><th>Minimum Volunteers</th><th>Maximum Volunteers</th><th>Description</th><th>Tools requested</th><th>Comments</th><th>Come if it rains?</th><th>Describe the project we would do on a rainy day</th><th>Restrictions</th><th>Category</th><th>Edit Link</th><th>Cancel Project</th></tr>";
     while ($row = $result->fetch_assoc())  {
       echo "<tr>";
       echo "<td style='text-align:center'>".$row["address"]."</td>";
@@ -44,9 +44,8 @@
       echo "<td style='text-align:center'>".$row["rain_proj"]."</td>";
       echo "<td style='text-align:center'>".$row["restrictions"]."</td>";
       echo "<td style='text-align:center'>".$row["category"]."</td>";
-      echo "</tr>";
-      // echo "<td>&nbsp;<a href='edit.php?vol_number=".urlencode($row["vol_number"])."'>Edit</a>&nbsp;&nbsp;</td>";
-      // echo "<td>&nbsp;<a href='delete.php?vol_number=".urlencode($row["vol_number"])." ' onclick='return confirm('Are you sure?');'>Delete</a>&nbsp;&nbsp;</td>";
+      echo "<td>&nbsp;<a href='editProject.php?project=".urlencode($row["Project_Number"])."'>Edit</a>&nbsp;&nbsp;</td>";
+      echo "<td>&nbsp;<a href='deleteProject.php?project=".urlencode($row["Project_Number"])."' onclick='return confirm('Are you sure?');'>Delete</a>&nbsp;&nbsp;</td>";
       echo "</tr>";
 		}
 		echo "</table>";
@@ -54,7 +53,7 @@
 		echo "</div>";
 	}
   echo "<center>";
-  echo "<br /><br /><a href='select.php'>Register a Volunteer</a>";
+  echo "<br /><br /><a href='registerProject.php'>Register a Project</a>";
   echo "</center>";
 ?>
 
