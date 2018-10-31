e597f2a040b09fed1287367b8135b8e859bef018
Abby Garrett <agarrett@go.olemiss.edu>
2018-10-31 14:34:04 -0500

    Update viewHostProjects.php

--- viewHostProjects.php
+++ viewHostProjects.php
@@ -16,7 +16,7 @@
 		$last=$row["last_name"];
 	}
 
-  $query="SELECT Project_Number, address, min_volunteers AS 'min', ";
+  $query="SELECT Project_Number, Host, address, min_volunteers AS 'min', ";
   $query.="max_volunteers AS 'max', description, tools, additional_comments AS 'comments', ";
   $query.="rain, rain_proj, ";
   $query.="restriction_violation AS 'restrictions', proj_category.cat AS 'category' from Projects ";
@@ -27,12 +27,15 @@
 /********************    Uncomment Once Code Completed  **************************/
 
 	if ($result && $result->num_rows > 0) {
-		echo "<div class='row'>";
+		echo "<div>";
 		echo "<center>";
     echo "<h2>Here are all projects for ".$first." ".$last."</h2>";
-    echo "<table>";
-    echo "<tr><th>Address</th><th>Minimum Volunteers</th><th>Maximum Volunteers</th><th>Description</th><th>Tools requested</th><th>Comments</th><th>Come if it rains?</th><th>Describe the project we would do on a rainy day</th><th>Restrictions</th><th>Category</th><th>Edit Link</th><th>Cancel Project</th></tr>";
-    while ($row = $result->fetch_assoc())  {
+		echo "<table id='projects' class='table table-striped table-bordered table-sm' cellspacing='0' width='100%'>";
+		echo "<thead>";
+		echo "<tr><th>Address</th><th class='th-sm'>Minimum Volunteers<i class='fa fa-sort float-right' aria-hidden='true'></i></th><th class='th-sm'>Maximum Volunteers<i class='fa fa-sort float-right' aria-hidden='true'></i></th><th>Description</th><th>Tools requested</th><th>Comments</th><th class='th-sm'>Come if it rains?<i class='fa fa-sort float-right' aria-hidden='true'></i></th><th>Describe the project we would do on a rainy day</th><th class='th-sm'>Restrictions<i class='fa fa-sort float-right' aria-hidden='true'></i></th><th class='th-sm'>Category</th><th>Edit Link</th><th>Cancel Project</th></tr>";
+		echo "</thead>";
+		echo "<tbody>";
+		while ($row = $result->fetch_assoc())  {
       echo "<tr>";
       echo "<td style='text-align:center'>".$row["address"]."</td>";
       echo "<td style='text-align:center'>".$row["min"]."</td>";
@@ -44,17 +47,21 @@
       echo "<td style='text-align:center'>".$row["rain_proj"]."</td>";
       echo "<td style='text-align:center'>".$row["restrictions"]."</td>";
       echo "<td style='text-align:center'>".$row["category"]."</td>";
-      echo "<td>&nbsp;<a href='editProject.php?project=".urlencode($row["Project_Number"])."'>Edit</a>&nbsp;&nbsp;</td>";
+			echo "<td>&nbsp;<a href='editProject.php?project=".$row["Project_Number"]."&host=".$row['Host']."'>Edit</a>&nbsp;&nbsp;</td>";
       echo "<td>&nbsp;<a href='deleteProject.php?project=".urlencode($row["Project_Number"])."' onclick='return confirm('Are you sure?');'>Delete</a>&nbsp;&nbsp;</td>";
       echo "</tr>";
 		}
+		echo "</tbody>";
 		echo "</table>";
 		echo "</center>";
 		echo "</div>";
 	}
   echo "<center>";
-  echo "<br /><br /><a href='registerProject.php'>Register a Project</a>";
+	echo "<br /><br /><a href='editHost.php?host=".$host_num."'>Edit Host Info</a>";
+  echo "<br /><br /><a href='registerProject.php?host=".$host_num."'>Register a Project</a>";
   echo "</center>";
+
+	echo "<br /><p>&laquo:<a href='index.php'>Back to Main Page</a>";
 ?>
 
 <?php  new_footer("Volunteers", $mysqli); ?>
