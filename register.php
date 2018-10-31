cac4b5e03c9b564fa19822ee0433f588c6f9609e
Abby Garrett <agarrett@go.olemiss.edu>
2018-10-31 14:32:02 -0500

    Update register.php

--- register.php
+++ register.php
@@ -16,29 +16,25 @@
 	//condition to check if you submited something
   if (isset($_POST["submit"])) {
 		//makes sure you filled in all boxes
-		if( (isset($_POST["studentID"]) && $_POST["studentID"] !== "") && (isset($_POST["fname"]) && $_POST["fname"] !== "") && (isset($_POST["lname"]) && $_POST["lname"] !== "") &&(isset($_POST["email"]) && $_POST["email"] !== "") &&(isset($_POST["transport"]) && $_POST["transport"] !== "") && (isset($_POST["restrict"]) && $_POST["restrict"] !== "") && (isset($_POST["phone"]) && $_POST["phone"] !== "") && (isset($_POST["org"]) && $_POST["org"] !== "")){
+		if( (isset($_POST["studentID"]) && $_POST["studentID"] !== "") && (isset($_POST["webID"]) && $_POST["webID"] !== "") && (isset($_POST["fname"]) && $_POST["fname"] !== "") && (isset($_POST["lname"]) && $_POST["lname"] !== "") &&(isset($_POST["email"]) && $_POST["email"] !== "") &&(isset($_POST["transport"]) && $_POST["transport"] !== "") && (isset($_POST["phone"]) && $_POST["phone"] !== "") && (isset($_POST["org"]) && $_POST["org"] !== "")){
 
-				//creates query to find the max volunteer number in order to later increment it
-        $max = "Select max(vol_number) AS max from volunteers";
-        $result2 = $mysqli->query($max);
-				//sets max2 to max+ 1 so that that will be the new volunteer number
-        if($result2 && $result2->num_rows >= 1){
-          while($row=$result2->fetch_assoc()){
-            $max2 = $row['max'] + 1;
-          }
-        }
+				$restriction = "";
+				foreach($_POST['restrict'] as $selected){
+					$restriction .= $selected.", ";
+				}
 				//create query to insert the person into the database
 				//(no project number assigned because that does not occur at registration)
 				$query = "INSERT INTO volunteers ";
-				$query .= "(vol_number, first_name, last_name, email, provide_transport, restrictions, phone_numbers, organization) ";
+				$query .= "(umID, webID, first_name, last_name, email, provide_transport, restrictions, phone_numbers, organization) ";
 				$query.="VALUES (";
         $query.=$_POST["studentID"].", ";
+				$query.="'".$_POST["webID"]."', ";
 				$query.="'".$_POST["fname"]."', ";
 				$query.="'".$_POST["lname"]."', ";
         $query.="'".$_POST["email"]."', ";
 				$query.="'".$_POST["transport"]."', ";
-				$query.="'".$_POST["restrict"]."', ";
-				$query.="'".$_POST["phone"]."', ";
+				$query.="'".$restriction."', ";
+				$query.=preg_replace('/[^0-9]/', '', $_POST["phone"]).", ";
 				$query.="(Select number from Organization WHERE name = '".$_POST["org"]."'))";
 
 				//execute query
@@ -48,13 +44,15 @@
 		if($result) {
 			//if added to the database posts and redirects to volunteer table
 			$_SESSION["message"] = $_POST["fname"]." ".$_POST["lname"]." has been registered";
-				header("Location: volunteer.php");
+				header("Location: index.php");
 				exit;
 
 			}
 			else {
 				//if a problem occured with adding to the database
-			$_SESSION["message"] = "Error! Could not change ".$_POST["fname"]." ".$_POST["lname"];
+				$_SESSION["message"] = "Error! Could not add ".$_POST["fname"]." ".$_POST["lname"];
+				header("Location: register.php");
+				exit;
 			}
 		}
 		else {
@@ -66,9 +64,11 @@
 	}
 	else {
 		//creates form
-			echo "<form method='POST' action='register.php'>";
+			echo "<form method='POST' action='register.php' style='padding-left: 8%; max-width: 50%;'>";
 
 						echo "StudentID Number:<input type='text' name='studentID' value='' />";
+
+						echo "WebID:<input type='text' name='webID' value='' />";
 
 						echo "First Name:<input type='text' name='fname' value='' />";
 
@@ -84,11 +84,24 @@
               echo "<option value='no'>No</option>";
             echo "</select>";
 
-            echo "Physical restrictions:<select name='restrict'>";
-              echo "<option value='NA'>None</option>";
-              echo "<option value='allergy'>Outdoor alergies</option>";
-              echo "<option value='handicap'>Physical disability (prevents heavy lifting projects)</option>";
-            echo "</select>";
+						echo "<br />";
+						echo "<br />";
+
+            echo "Physical restrictions:";
+						echo "<br />";
+							// options for organizations to select gathered from the database
+								echo "<INPUT TYPE='checkbox' Name='restrict[]' value = 'allergies'>Outdoor Allergies";
+								echo "<br />";
+								echo "<INPUT TYPE='checkbox' Name='restrict[]' value = 'disable'>Physical Disability";
+								echo "<br />";
+								echo "<INPUT TYPE='checkbox' Name='restrict[]' value = 'no heights'>Not comfortable with heights";
+								echo "<br />";
+								echo "<INPUT TYPE='checkbox' Name='restrict[]' value = 'no tools'>Uncomfortable with power tools";
+								echo "<br />";
+
+						echo "</select>";
+
+						echo "<br />";
 
             echo "Select your organization:<select name='org'>";
               // options for organizations to select gathered from the database
@@ -111,7 +124,7 @@
 	echo "</label>";
 	echo "</div>";
 	//adds link back to main page where you can navigate to what you want to do
-	echo "<br /><p>&laquo:<a href='bigevent.php'>Back to Main Page</a>";
+	echo "<br /><p>&laquo:<a href='index.php'>Back to Main Page</a>";
 ?>
 
 
