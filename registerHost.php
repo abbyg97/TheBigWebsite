27c1f1898f11e4806c545f12e2df11997bab14a0
Abby Garrett <agarrett@go.olemiss.edu>
2018-10-31 14:32:43 -0500

    Update registerHost.php

--- registerHost.php
+++ registerHost.php
@@ -18,22 +18,13 @@
 		//makes sure you filled in all boxes
 		if( (isset($_POST["fname"]) && $_POST["fname"] !== "") && (isset($_POST["lname"]) && $_POST["lname"] !== "") && (isset($_POST["number"]) && $_POST["number"] !== "") &&(isset($_POST["sec_num"]) && $_POST["sec_num"] !== "") && (isset($_POST["email"]) && $_POST["email"] !== "") && (isset($_POST["alt_fname"]) && $_POST["alt_fname"] !== "") && (isset($_POST["alt_lname"]) && $_POST["alt_lname"] !== "") && (isset($_POST["alt_phone"]) && $_POST["alt_phone"] !== "") && (isset($_POST["alt_email"]) && $_POST["alt_email"] !== "")){
 
-				//creates query to find the max volunteer number in order to later increment it
-        $max = "Select max(host_number) AS max from Host";
-        $result2 = $mysqli->query($max);
-				//sets max2 to max+ 1 so that that will be the new volunteer number
-        if($result2 && $result2->num_rows >= 1){
-          while($row=$result2->fetch_assoc()){
-            $max2 = $row['max'] + 1;
-          }
-        }
 				//create query to insert the person into the database
 				//(no project number assigned because that does not occur at registration)
 				//https://stackoverflow.com/questions/6278296/extract-numbers-from-a-string -- for formatting phone number
 				$query = "INSERT INTO Host ";
-				$query .= "(host_number, first_name, last_name, phone, second_phone, email, alt_first_name, alt_last_name, alt_phone, alt_email) ";
+				$query .= "(first_name, last_name, phone, second_phone, email, alt_first_name, alt_last_name, alt_phone, alt_email) ";
 				$query.="VALUES (";
-        $query.=$max2.", ";
+        // $query.=$max2.", ";
 				$query.="'".$_POST["fname"]."', ";
 				$query.="'".$_POST["lname"]."', ";
         $query.="".preg_replace('/[^0-9]/', '', $_POST["number"]).", ";
@@ -51,28 +42,110 @@
 		if($result) {
 			//if added to the database posts and redirects to volunteer table
 			// $_SESSION["message"] = $_POST["fname"]." ".$_POST["lname"]." has been registered";
-				redirect_to("viewHostProjects.php?id=".$max2);
-				exit;
+			if (isset($_POST["username"]) && $_POST["username"] !== "" && isset($_POST["password"]) &&
+					$_POST["password"] !== "") {
+					//Grab posted values for username and password. Immediately encrypt the password so that
+					// it is set up to compare with the encrypted password in the database Use password_encrypt
+					$username = $_POST["username"];
+					$password = password_encrypt($_POST["password"]);
+					//Check to make sure user does not already exist
+					$query = "SELECT * FROM ";
+					$query .= "hostLogin WHERE ";
+					$query .= "username = '".$username."' ";
+					$query .= "LIMIT 1";
+					$result = $mysqli->query($query);
+					//User exists so output that the user already exists
+					if ($result && $result->num_rows > 0) {
+						$_SESSION["message"] = "The username already exists";
+						redirect_to("addLogin.php");
+					}
+					//User does not already exist so add to admins table
+					else {
+						$query2="Select host_number from Host where first_name=".$_POST["first_name"]." and last_name=".$POST_["last_name"];
+						$result2 = $mysqli->query($query2);
+						while($row2=$result2->fetch_assoc()){
+							$number = $row2['host_number'];
+						}
+						$query = "INSERT INTO hostLogin ";
+						$query .= "(username, password, permissions, host_number) ";
+						//ended here
+						$query .= "VALUES ('".$username."', '".$password."', 3, ".$number.")";
+						$result = $mysqli->query($query);
+						if ($result) {
+							// $_SESSION["message"] = "User successfully added";
+							// redirect_to("index.php");
+							redirect_to("viewHostProjects.php?id=".$number);
+							exit;
+						}
+						else {
+							$_SESSION["message"] = "Could not add user!";
+							redirect_to("addLogin.php");
+							exit;
+					}
+				}
+
+
 			}
 			else {
 				//if a problem occured with adding to the database
 			$_SESSION["message"] = "Error! Could not register ".$_POST["fname"]." ".$_POST["lname"];
+			redirect_to("viewHostProjects.php?id=".$max2);
+			exit;
 			}
 		}
+	}
 		else {
 			//sets message to remind you to fill in all boxes if you forgot one
 			$_SESSION["message"] = "Unable to add person. Fill in all information!";
-			header("Location: registerHost.php");
-			exit;
+
+			echo "<form method='POST' action='registerHost.php' style='padding-left: 15%;'>";
+
+						echo "First Name:<input type='text' name='fname' value='".$_POST['fname']."' />";
+
+						echo "Last Name:<input type='text' name='lname' value='".$_POST['lname']."' />";
+
+						echo "Username:<input type='text' name='username' value='".$_POST['username']."' />";
+
+						echo "Password:<input type='password' name='password' value='' />";
+
+						echo "Phone Number:<input type='text' name='number' value='".$_POST['number']."' />";
+
+						echo "Second Number:<input type='text' name='sec_num' value='".$_POST['sec_num']."' />";
+
+						echo "Email:<input type='text' name='email' value='".$_POST['email']."' />";
+
+            echo "Please enter alternate contact information  <br></br>";
+
+            echo "First Name:<input type='text' name='alt_fname' value='".$_POST['alt_fname']."' />";
+
+						echo "Last Name:<input type='text' name='alt_lname' value='".$_POST['alt_lname']."' />";
+
+						echo "Phone Number:<input type='text' name='alt_phone' value='".$_POST['alt_phone']."' />";
+
+						echo "Email:<input type='text' name='alt_email' value='".$_POST['alt_email']."' />";
+
+					echo "<input type='submit' name='submit' class='button tiny round' value='Register'>";
+
+
+    echo "</form>";
+		//header("Location: registerHost.php");
+			//exit;
+
 		}
 	}
+
 	else {
 		//creates form
-			echo "<form method='POST' action='registerHost.php'>";
+
+			echo "<form method='POST' action='registerHost.php' style='padding-left: 15%;'>";
 
 						echo "First Name:<input type='text' name='fname' value='' />";
 
 						echo "Last Name:<input type='text' name='lname' value='' />";
+
+						echo "Username:<input type='text' name='username' value='' />";
+
+						echo "Password:<input type='password' name='password' value='' />";
 
 						echo "Phone Number:<input type='text' name='number' value='' />";
 
@@ -100,7 +173,7 @@
 	echo "</label>";
 	echo "</div>";
 	//adds link back to main page where you can navigate to what you want to do
-	echo "<br /><p>&laquo:<a href='bigevent.php'>Back to Main Page</a>";
+	echo "<br /><p>&laquo:<a href='index.php'>Back to Main Page</a>";
 ?>
 
 
